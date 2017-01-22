28dcb06a22fb1b2ce0eb200af9835f365039f558
Mark Keller <markkeller@Marks-MacBook-Pro-2.local>
2017-01-22 04:28:56 -0500

    added sign in

--- frontend.php
+++ frontend.php
@@ -3,3 +3,7 @@
 Flight::route('/', function(){
   Flight::render('index.php');
 });
+
+Flight::route('/sign-in', function(){
+  Flight::render('signin.php');
+});
---
+++ views/signin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="/">
+  First name:<br>
+  <input type="text" name="Employee no." value="xxxx-xxxx-xxxx-xxxx">
+  <br>
+  <input type="submit" value="Submit">
+</form>
+
+<p>Swipe to see inventory.</p>
+
+</body>
+</html>
